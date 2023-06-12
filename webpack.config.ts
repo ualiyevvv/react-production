04f4dd05ce99947d44f6e9a3ca46aa3c330d18ab
Ayan <xchjjvg07@gmail.com>
2023-06-12 21:10:01 +0600

    webpack config decomposition

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,40 +1,24 @@
 import path from 'path';
 import webpack from 'webpack';
-import webpackDevServer from 'webpack-dev-server';
-import HTMLWebpackPlugin from 'html-webpack-plugin';
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
+import buildWebpackConfig from "./config/build/buildWebpackConfig";
 
-const config: webpack.Configuration = {
-    mode: "development",
-    entry: path.resolve(__dirname,'src','index.ts'),
-    devtool: 'inline-source-map',
-    devServer:  {
-        port: 9001,
-        open: true,
-    },
-    output: {
-        filename: "[name].[contenthash].js",
-        path: path.resolve(__dirname, 'build'),
-        clean: true,
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html'),
-        }),
-        new webpack.ProgressPlugin(),
+export default (env: BuildEnv) => {
+    const paths: BuildPaths = {
+        entry: path.resolve(__dirname,'src','index.ts'),
+        build: path.resolve(__dirname, 'build'),
+        html: path.resolve(__dirname, 'public', 'index.html'),
+    }
 
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-}
+    const mode = env.mode || 'development';
+    const PORT = env.port || 9001;
 
-export default config;
+    const isDev = mode === 'development';
+
+    return buildWebpackConfig({
+        mode,
+        paths,
+        isDev,
+        port: PORT
+    });
+};
