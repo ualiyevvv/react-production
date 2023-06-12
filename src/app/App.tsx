a870c300113688313af344114bec79ad8d689de0
Ayan <xchjjvg07@gmail.com>
2023-06-13 00:02:41 +0600

    featured sliced design added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import {Link, Route, Routes} from 'react-router-dom';
-import {AboutPageAsync} from "pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "pages/MainPage/MainPage.async";
-import {Suspense, useContext, useState} from "react";
+import {Suspense} from "react";
 import './styles/index.scss';
 
-import {classNames} from "helpers/classNames";
+import {classNames} from "shared/lib/helpers/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPageAsync} from "pages/AboutPage";
+import {MainPageAsync} from "pages/MainPage";
 
 export const App = () => {
     const {theme, toggleTheme} = useTheme();
--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,4 +1,4 @@
-import MainPage from "../MainPage/MainPage";
+import MainPage from "../../MainPage/ui/MainPage";
 
  const AboutPage = () => {
     return (<>
